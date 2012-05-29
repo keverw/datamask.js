bdcc6847bdd5d445b9686e330491d17d150a7884
Kevin <keverw92@gmail.com>
2012-05-29 15:20:21 -0400

    email function is done!

--- datamask.js
+++ datamask.js
@@ -1,18 +1,30 @@
 (function ()
 {
-    function string(string, mask_char, percent)
+    function string(value, mask_char, percent)
     {
-
+    	return value;
     }
 
-    function domain(domain, mask_char, percent)
+    function domain(value, mask_char, percent)
     {
-
+		return value;
     }
 
-    function email(email)
+    function email(address)
     {
-
+    	if (address.indexOf('@') !== -1) //has @ symbol
+    	{
+    		var email_parts = address.split('@');
+    		
+    		var masked_user = string(email_parts[0], '*', 50);
+			var masked_domain = domain(email_parts[1],'*' , 60);
+    		return(masked_user + '@' + masked_domain);
+    	}
+    	else //else no at, just mask the whole input
+    	{
+    		return string(address, '*', 50);
+    	}
+		
     }
 
     // Export public API
